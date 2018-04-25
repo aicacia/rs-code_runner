0c3be3d02175e04072835292447f8b4c5d5876f1
Nathan Faucett <nathanfaucett@gmail.com>
2018-04-24 19:52:38 -0500

    fix docker write empty line to stdin bug

--- README.md
+++ README.md
@@ -31,8 +31,18 @@ takes json in the form
 
 ## Call from Command line
 
+### Rust
+
 ```bash
 $ runner
 {"language": "rust", "files": [{"name": "main.rs","content": "fn main() { println!(\"Hello, world\"); }"}], "argv": []}
+{"stdout":"Hello, world\n","stderr":"","error":null}
+```
+
+### ECMAScript/JavaScript
+
+```bash
+$ runner
+{"language": "ecmascript", "files": [{"name": "main.js","content": "console.log(\"Hello, world!\");"}], "argv": []}
 {"stdout":"Hello, world\n","stderr":"","error":null}
 ```
--- src/main.rs
+++ src/main.rs
@@ -5,23 +5,25 @@ extern crate serde_json;
 use runner::Output;
 use std::io;
 
-fn main() {
+fn main() -> io::Result<()> {
     let mut input_json = String::new();
 
-    match io::stdin().read_line(&mut input_json) {
-        Ok(_) => match serde_json::from_str(&input_json) {
-            Ok(input) => match runner::run(&input) {
-                Ok(output) => println!("{}", serde_json::to_string(&Output::from(output)).unwrap()),
-                Err(error) => println!("{}", serde_json::to_string(&Output::from(error)).unwrap()),
-            },
-            Err(error) => println!(
-                "{}",
-                serde_json::to_string(&Output::from(error.to_string())).unwrap()
-            ),
+    // Docker writes an empty string to stdin
+    while input_json.trim().is_empty() {
+        input_json.clear();
+        io::stdin().read_line(&mut input_json)?;
+    }
+
+    match serde_json::from_str(input_json.trim()) {
+        Ok(input) => match runner::run(&input) {
+            Ok(output) => println!("{}", serde_json::to_string(&Output::from(output)).unwrap()),
+            Err(error) => println!("{}", serde_json::to_string(&Output::from(error)).unwrap()),
         },
         Err(error) => println!(
             "{}",
             serde_json::to_string(&Output::from(error.to_string())).unwrap()
         ),
     }
+
+    Ok(())
 }
